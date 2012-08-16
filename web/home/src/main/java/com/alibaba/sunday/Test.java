79d3168fed5f1824894b209453bc632484390d9d
xurik <sunday.xuri@gmail.com>
2012-08-16 19:28:36 -0400

    test

---
+++ web/home/src/main/java/com/alibaba/sunday/Test.java
@@ -0,0 +1,11 @@
+package com.alibaba.sunday;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sunday
+ * Date: 8/16/12
+ * Time: 3:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
