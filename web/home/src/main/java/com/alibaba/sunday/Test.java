d586f4e232b1faca7a9eaedcbebee8addcb2f49e
xurik <sunday.xuri@gmail.com>
2012-08-16 19:34:30 -0400

    test

--- web/home/src/main/java/com/alibaba/sunday/Test.java
+++ web/home/src/main/java/com/alibaba/sunday/Test.java
@@ -1,5 +1,10 @@
 package com.alibaba.sunday;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created with IntelliJ IDEA.
  * User: sunday
@@ -7,5 +12,15 @@ package com.alibaba.sunday;
  * Time: 3:40 PM
  * To change this template use File | Settings | File Templates.
  */
+@Controller
 public class Test {
+    @RequestMapping("/test1")
+    public ModelAndView test(){
+        ModelAndView view = new ModelAndView("test111");
+        StringUtils.split("---");
+        view.addObject("test","test");
+        System.out.println("acccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaacccccc");
+        System.out.println("-----------------*********--------------------");
+        return view;
+    }
 }
