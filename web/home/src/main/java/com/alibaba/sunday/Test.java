2f57f0644de47069fb9199893206031a9dfb6aef
xurik <sunday.xuri@gmail.com>
2012-08-16 21:30:29 -0400

    test

--- web/home/src/main/java/com/alibaba/sunday/Test.java
+++ web/home/src/main/java/com/alibaba/sunday/Test.java
@@ -1,9 +1,15 @@
 package com.alibaba.sunday;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.lang.StringUtils;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.SQLException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,13 +20,23 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class Test {
+    @Autowired
+    @Qualifier("dataSource")
+    private BasicDataSource dataSource;
+    @Autowired
+    @Qualifier("sessionFactory")
+    private SessionFactory sessionFactory;
     @RequestMapping("/test1")
     public ModelAndView test(){
         ModelAndView view = new ModelAndView("test111");
         StringUtils.split("---");
         view.addObject("test","test");
-        System.out.println("acccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaacccccc");
-        System.out.println("-----------------*********--------------------");
+        System.out.println(sessionFactory.getCache()+"-----------------------------------");
+        try {
+            System.out.println(dataSource.getConnection().getCatalog());
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
         return view;
     }
 }
